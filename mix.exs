b76e0e42bbdacae63b90cf9ef0325b2a60db9863
Kurtis Rainbolt-Greene <kurtis@rainbolt-greene.online>
2025-01-06 02:20:53 -0800

    Upgrade to Phoenix LiveView 1.0.*

--- mix.exs
+++ mix.exs
@@ -86,7 +86,7 @@ defmodule Plox.MixProject do
     [
       {:decimal, "~> 2.0"},
       {:ex_doc, "~> 0.31", only: :dev, runtime: false},
-      {:phoenix_live_view, "~> 0.20"},
+      {:phoenix_live_view, "~> 1.0"},
       {:styler, "~> 0.11", only: [:dev, :test], runtime: false}
     ]
   end
