eb5a79fab472f76b4802537e2ca75947f10f1156
Nikki Kyllonen <nikki-kyllonen@comcast.net>
2025-07-18 11:37:24 -0700

    Bump Erlang and Elixir versions for CI

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Plox.MixProject do
     [
       app: :plox,
       version: @version,
-      elixir: "~> 1.15",
+      elixir: "~> 1.18",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
