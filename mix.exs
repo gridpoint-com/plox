c75dd9f49fd123cdbd52dee8f51614344aa790fb
Chris Dosé <chris@xn--dos-dma.com>
2025-07-23 13:43:20 -0700

    Run CI against more elixir/OTP versions (#22)
    * Run CI against more elixir/OTP versions
    
    * Run CI on all pushes and PRs, not just against main
    
    * Remove fancy new guard so we can support older Elixir

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Plox.MixProject do
     [
       app: :plox,
       version: @version,
-      elixir: "~> 1.18",
+      elixir: "~> 1.16",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
 
