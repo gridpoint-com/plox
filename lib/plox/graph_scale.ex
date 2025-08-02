c19ded63fbfffb2d460c4b8cee3232afd4867bbf
Jason Johnson <jasonmj@gmail.com>
2025-08-02 00:01:16 -0400

    Update deprecated range usage

--- lib/plox/graph_scale.ex
+++ lib/plox/graph_scale.ex
@@ -15,24 +15,18 @@ defmodule Plox.GraphScale do
   def values(%__MODULE__{scale: scale}, opts \\ %{}), do: Scale.values(scale, opts)
 
   def to_graph_x(%__MODULE__{scale: scale, dimensions: dimensions}, value) do
-    Scale.convert_to_range(
-      scale,
-      value,
-      (dimensions.margin.left +
-         dimensions.padding.left)..(dimensions.width -
-                                      dimensions.margin.right -
-                                      dimensions.padding.right)
-    )
+    first = dimensions.margin.left + dimensions.padding.left
+    last = dimensions.width - dimensions.margin.right - dimensions.padding.right
+    step = if first < last, do: 1, else: -1
+
+    Scale.convert_to_range(scale, value, first..last//step)
   end
 
   def to_graph_y(%__MODULE__{scale: scale, dimensions: dimensions}, value) do
-    Scale.convert_to_range(
-      scale,
-      value,
-      (dimensions.height -
-         dimensions.margin.bottom -
-         dimensions.padding.bottom)..(dimensions.margin.top +
-                                        dimensions.padding.top)
-    )
+    first = dimensions.height - dimensions.margin.bottom - dimensions.padding.bottom
+    last = dimensions.margin.top + dimensions.padding.top
+    step = if first < last, do: 1, else: -1
+
+    Scale.convert_to_range(scale, value, first..last//step)
   end
 end
--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule Plox.MixProject do
   use Mix.Project
 
-  @version "0.2.0"
+  @version "0.2.1"
   @source_url "https://github.com/gridpoint-com/plox"
 
   def project do
